1469709d3a26dff639547cea789b410a2e6d64f3
Michael Severance <mikeseve@vt.edu>
2019-04-25 15:14:39 -0400

    updatedAddStopPage changed date time on create Ride html, finished main page formatting and scroll bar with cookies, added riderWaiting page and moved some functions to utils.js file

--- mainPage.js
+++ mainPage.js
@@ -1,51 +1,32 @@
-function myFunction() {
-
-  init();
-}
-
-function loadJSON(callback) {
-
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'http://time.jsontest.com', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);
- }
-
- function reply_click(id)
- {
-   var card = document.getElementById(id).children[0].children[0].children[3];
-   //var c = document.getElementById("myDIV").children;
-   console.log(card.innerHTML);
- }
-
 function init() {
- loadJSON(function(response) {
+  createCookie("test", "value", -1);
+  loadJSON(function(response) {
     //var actual_JSON = JSON.parse(response);
 
-    var driverOneData = '{"driverId":"driverOne","StartingPoint":"10N,70W", "Destination":"15N,50W", "DepartureTime":"4:15 4/24/19"}'
+    var driverOneData = '{"routeId":"1","StartingPoint":"10N,70W", "Destination":"15N,50W", "DepartureTime":"4:15 4/24/19"}'
 
-    var driverTwoData = '{"driverId":"driverTwo","StartingPoint":"70N,70W", "Destination":"20N,50W", "DepartureTime":"4:15 4/24/19"}'
+    var driverTwoData = '{"routeId":"2","StartingPoint":"70N,70W", "Destination":"20N,50W", "DepartureTime":"4:15 4/24/19"}'
 
-    var driverThreeData = '{"driverId":"driverThree","StartingPoint":"100N,70W", "Destination":"150N,50W", "DepartureTime":"4:15 4/24/19"}'
+    var driverThreeData = '{"routeId":"3","StartingPoint":"100N,70W", "Destination":"150N,50W", "DepartureTime":"4:15 4/24/19"}'
 
-
-
-    var totalJson = '{"data":[' + driverOneData + ',' + driverTwoData + ',' + driverThreeData + ']}';
+    //var totalJson = '{"data":[' + driverOneData + ',' + driverTwoData + ',' + driverTwoData+',' + driverTwoData + ','+ driverTwoData + ','+ driverTwoData + ','+ driverTwoData + ','+ driverTwoData + ','+ driverTwoData + ','+ driverThreeData + ']}';
+    var totalJson = '{"data":[' + driverOneData + ',' + driverThreeData + ']}';
     console.log(totalJson);
     var actual_JSON = JSON.parse(totalJson);
 
     //console.log(actual_JSON)
     for (i in actual_JSON.data) {
       var node = document.createElement("DIV");
-      node.innerHTML = '<div class="overflowContainer" id="rideCardId'+i+'" onClick="reply_click(this.id)"><div class="card"><div class="driveContainer"><div class="cardElement">Starting Point: '+actual_JSON.data[i].StartingPoint+' </div><div class="cardElement">Destination: '+actual_JSON.data[i].Destination+' </div><div class="cardElement">Departure Time: '+actual_JSON.data[i].DepartureTime+'</div><p hidden>'+actual_JSON.data[i].driverId+'</p></div></div></div>'
+      node.innerHTML = '<div class="overflowContainer" id="rideCardId'+i+'" onClick="reply_click(this.id)"><div class="card"><div class="driveContainer"><div class="cardElement">Starting Point: '+actual_JSON.data[i].StartingPoint+' </div><div class="cardElement">Destination: '+actual_JSON.data[i].Destination+' </div><div class="cardElement">Departure Time: '+actual_JSON.data[i].DepartureTime+'</div><p hidden>'+actual_JSON.data[i].routeId+'</p></div></div></div>'
       document.getElementById("driverList").appendChild(node);
 
     }
  });
 }
+
+ function reply_click(id)
+ {
+   var card = document.getElementById(id).children[0].children[0].children[3];
+   createCookie("routeId", card.innerHTML, 100);
+   window.location.href = "./addStop.html";
+ }
---
+++ utils.js
@@ -0,0 +1,39 @@
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function createCookie(name,value,days) {
+     if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+     }
+     else var expires = "";
+     document.cookie = name+"="+value+expires+";";
+}
+
+function loadJSON(callback) {
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'http://time.jsontest.com', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);
+ }
