94b812dd2063d49444e8f88cb423e935909b5cc6
1CharlesStern <cstern1@vt.edu>
2019-04-25 19:04:30 -0400

    Test cases complete

--- test_app.py
+++ test_app.py
@@ -3,16 +3,12 @@
 import server
 import sqlite3
 
+#TODO: Determine if database is purged between tests.  Assuming that it is for now.
+
 client = server.app.test_client()
-dt = client.post('/createUser', data=dict(
-        username='username',
-        email='joe@example.com',
-        password='passw0rd',
-        password2='passw0rd'
-    ), follow_redirects=True)
-print(dt)
 
 #TEST LOGIN
+#"Login" button should take the user to the main page if credentials are correct
 def test_good_redirect():
     client.post('/createUser', data=dict(
         username='username',
@@ -25,20 +21,37 @@ def test_good_redirect():
         password='passw0rd',
     ), follow_redirects=True)
     assertEqual(dt.status_code, 200)
+#"Login" button should display an error if the credentials are not correct
 def test_bad_redirect():
     dt = client.post('/login', data=dict(
         username='username',
         password='passw0rd',
     ), follow_redirects=True)
     assertEqual(dt.status_code, 400)
+#When a user logs in with correct credentials, they should recieve a valid token
 def test_good_getToken():
-    return
+    dt = client.post('/createUser', data=dict(
+        username='username',
+        email='joe@example.com',
+        password='passw0rd',
+        password2='passw0rd'
+    ), follow_redirects=True)
+    assert 'username' in dt.data
+#When a user logs in with incorrect credentials, they should not recieve a token
 def test_bad_getToken():
-    return
+    client.post('/createUser', data=dict(
+        username='username',
+        email='XD',
+        password='pass',
+        password2='pass'
+    ), follow_redirects=True)
+    assert 'username' not in dt.data
+#assert create account button exists
 def test_login_create():
     dt = client.get('/login', follow_redirect=True)
     assert 'Login' in dt.data
 #TEST CREATE ACCOUNT
+#User should recieve an error if trying to make an account that has an occupied username
 def test_bad_username():
     #TODO update post data dict with correct field names
     dt = client.post('/createUser', data=dict(
@@ -54,6 +67,7 @@ def test_bad_username():
         password2='passw0rd'
     ), follow_redirects=True)
     assertEqual(dt.status_code, 400)
+#User should recieve an error if trying to make an account that has an occupied email address
 def test_bad_email():
     # TODO update post data dict with correct field names
     dt = client.post('/createUser', data=dict(
@@ -69,6 +83,7 @@ def test_bad_email():
         password2='passw0rd'
     ), follow_redirects=True)
     assertEqual(dt.status_code, 400)
+#User should recieve an error if trying to make an account that had an invalid email address
 def test_invalid_email():
     # TODO update post data dict with correct field names
     dt = client.post('/createUser', data=dict(
@@ -78,6 +93,7 @@ def test_invalid_email():
         password2='passw0rd'
     ), follow_redirects=True)
     assertEqual(dt.status_code, 400)
+#User should recieve an error if the given password is less than 8 characters
 def test_invalid_password():
     # TODO update post data dict with correct field names
     dt = client.post('/createUser', data=dict(
@@ -87,6 +103,7 @@ def test_invalid_password():
         password2='pass'
     ), follow_redirects=True)
     assertEqual(dt.status_code, 400)
+#User should recieve an error if the given password does not match the password confirmation field
 def test_no_password_match():
     # TODO update post data dict with correct field names
     dt = client.post('/createUser', data=dict(
@@ -96,6 +113,7 @@ def test_no_password_match():
         password2='passw0rd'
     ), follow_redirects=True)
     assertEqual(dt.status_code, 400)
+#User should not recieve an error if all fields are correct
 def test_create_main():
     # TODO update post data dict with correct field names
     dt = client.post('/createUser', data=dict(
@@ -105,15 +123,22 @@ def test_create_main():
         password2='passw0rd'
     ), follow_redirects=True)
     assertEqual(dt.status_code, 200)
+#assert create account button exists
 def test_create_login():
     dt = client.get('/createUser', follow_redirects=True)
     assert 'Create Account' in dt.data
+#assert cancel button exists
+def test_create_cancel():
+    dt = client.get('/createUser', follow_redirects=True)
+    assert 'Cancel' in dt.data
+#The User's record should appear in the database
 def test_new_db_record():
     conn = squlite3.connect('db.squlite')
     cur = conn.cursor()
     cur.execute('SELECT * FROM users')
     assert 'username' in cur.fetchone()
 #TEST INSERT RIDE
+#User should recieve an error if trying to enter an invalid start location
 def test_bad_start():
     dt = client.post('/createRide', data=dict(
         start='your mom',
@@ -122,6 +147,7 @@ def test_bad_start():
         time='10:00'
     ), follow_redirects = True)
     assertEqual(dt.status_code, 400)
+#User should recieve an error if trying to enter an invalid destination
 def test_bad_end():
     dt = client.post('/createRide', data=dict(
         start='10W',
@@ -130,6 +156,7 @@ def test_bad_end():
         time='10:00'
     ), follow_redirects = True)
     assertEqual(dt.status_code, 400)
+#User should recieve an error if the start and destination are identical
 def test_locations_different():
     # TODO update post data dict with correct field names
     dt = client.post('/createRide', data=dict(
@@ -139,6 +166,7 @@ def test_locations_different():
         time='10:00'
     ), follow_redirects = True)
     assertEqual(dt.status_code, 400)
+#The "Date" field should only allow dates to be entered
 def test_datefield_only_dates():
     # TODO update post data dict with correct field names
     dt = client.post('/createRide', data=dict(
@@ -148,6 +176,7 @@ def test_datefield_only_dates():
         time='10:00'
     ), follow_redirects = True)
     assertEqual(dt.status_code, 400)
+#The "Time" field should only allow times to be entered
 def test_timefield_only_times():
     # TODO update post data dict with correct field names
     dt = client.post('/createRide', data=dict(
@@ -157,47 +186,66 @@ def test_timefield_only_times():
         time='H'
     ), follow_redirects = True)
     assertEqual(dt.status_code, 400)
+#assert cancel button exists
 def test_cancel_but():
     dt = client.get('/createRide', follow_redirects = True)
     assert 'Cancel' in dt.data
+#assert submit button exists
 def test_submit_but():
     dt = client.get('/createRide', follow_redirects = True)
     assert 'Submit Request' in dt.data
 #TEST ADD STOP
+#User should recieve an error if trying to enter an invalid starting location
 def test_start_location_invalid():
-    dt = client.post('/createRide', data=dict(
+    dt = client.post('/addStop', data=dict(
         start='torg',
         end='10W',
         date='2019-07-22',
         time='14:20'
     ),follow_redirects=True)
     assertEqual(dt.status_code, 400)
+#User should recieve an error if trying to enter an invalid destination
 def test_end_location_invalid():
-    dt = client.post('/createRide', data=dict(
+    dt = client.post('/addStop', data=dict(
         start='10N',
         end='mcb',
         date='2019-07-21',
         time='10:20'
     ),follow_redirects=True)
     assertEqual(dt.status_code, 400)
+#User should recieve an error if the start and destination are identical
 def test_locations_equal():
-    dt = client.post('/createRide', data=dict(
+    dt = client.post('/addStop', data=dict(
         start='10N',
         end='10N',
         date='2019-08-22',
         time='13:30'
     ),follow_redirects=True)
     assertEqual(dt.status_code, 400)
+#assert cancel button exists
 def test_add_cancel():
-    dt = client.post('/createRide', data=dict(
+    client.post('/createUser', data=dict(
+        username='username',
+        email='joe@example.com',
+        password='passw0rd',
+        password2='passw0rd'
+    ), follow_redirects=True)
+    dt = client.post('/addStop', data=dict(
         start='10N',
         end='50E',
         date='2017-08-19',
         time='12:00'
     ))
     assertIn(b'Cancel', dt.data)
+#assert submit button exists
 def test_add_submit():
-    dt = client.post('/createRide', data=dict(
+    client.post('/createUser', data=dict(
+        username='username',
+        email='joe@example.com',
+        password='passw0rd',
+        password2='passw0rd'
+    ), follow_redirects=True)
+    dt = client.post('/addStop', data=dict(
         start='10N',
         end='50E',
         date='2017-08-19',
@@ -205,8 +253,10 @@ def test_add_submit():
     ))
     assertIn(b'Submit', dt.data)
 #TEST LOGOUT
+#token should no longer be valid
 def test_token_invalid():
     dt = client.get('/createRide', follow_redirect = True)
+<<<<<<< Updated upstream
     assert 'Submit Request' in dt.data
 
 #FIVE VALIDATION TEST CASES
@@ -223,6 +273,10 @@ def test_login():
     ), follow_redirects=True)
     assertEqual(dt.status_code, 200)
     assertIn(b'Available Rides', dt.data)
+=======
+    assert 'Submit Request' not in dt.data
+#logout should appear on all pages once logged in
+>>>>>>> Stashed changes
 def test_logout():
     # TODO update post data dict with correct field names
     client.post('/createUser', data=dict(
